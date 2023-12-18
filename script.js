70e3a92052a5171a5d456c0175707f9ab444710d
Angel Burgos <37665615+anhellburgos@users.noreply.github.com>
2023-12-17 21:21:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,32 +8,26 @@ function changeBasemap(basemapUrl) {
   if (currentBasemap) {
     map.removeLayer(currentBasemap);
   }
-  const tiles = L.tileLayer(basemapUrl, { attribution: '' });
-  tiles.addTo(map);
+  const tiles = L.tileLayer(basemapUrl, { attribution: '' }).addTo(map);
   currentBasemap = tiles;
 }
 
 // Función para mostrar las coordenadas del marcador
 function showMarkerCoordinates(marker) {
-  var position = marker.getLatLng();
-  marker.setPopupContent("<b>Coordenadas WGS 84</b><br>Latitud: " + position.lat.toFixed(6) + "<br>Longitud: " + position.lng.toFixed(6));
+  const position = marker.getLatLng();
+  marker.setPopupContent(`<b>Coordenadas WGS 84</b><br>Latitud: ${position.lat.toFixed(6)}<br>Longitud: ${position.lng.toFixed(6)}`);
   marker.openPopup();
 }
 
 // Función para cargar y agregar capas GeoJSON al mapa
 function loadGeoJSON(url, name, markerIcon = null) {
   return fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var layer;
+    .then(response => response.json())
+    .then(data => {
+      let layer;
       if (markerIcon) {
-        // Si se proporciona un icono de marcador, usarlo para esta capa
         layer = L.geoJSON(data, {
-          pointToLayer: function (feature, latlng) {
-            return L.marker(latlng, { icon: markerIcon });
-          }
+          pointToLayer: (feature, latlng) => L.marker(latlng, { icon: markerIcon })
         });
       } else {
         layer = L.geoJSON(data);
@@ -44,15 +38,11 @@ function loadGeoJSON(url, name, markerIcon = null) {
 
 // Función para inicializar el mapa
 function initMap() {
-  // Creamos el mapa
   map = L.map('mapid').setView([-1.724593, -79.552002], 6);
 
-  // Agregamos la capa base inicial
-  var streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap contributors' });
-  streets.addTo(map);
+  const streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap contributors' }).addTo(map);
   currentBasemap = streets;
 
-  // Cargar capas GeoJSON y agregarlas al control de capas
   Promise.all([
     loadGeoJSON('osos.geojson', 'Avistamiento de osos', new L.Icon({
       iconUrl: 'osito.png',
@@ -64,21 +54,15 @@ function initMap() {
     loadGeoJSON('areas_protegidas.geojson', 'Áreas Protegidas'),
     loadGeoJSON('provincias.geojson', 'Provincias'),
     loadGeoJSON('cantones.geojson', 'Cantones'),
-    loadGeoJSON('parroquias.geojson', 'Parroquias')    
-    
-
-  ]).then(function () {
+    loadGeoJSON('parroquias.geojson', 'Parroquias')
+  ]).then(() => {
     L.control.layers(null, overlayLayers, { collapsed: false }).addTo(map);
-
-    // Desactivar (unchecked) las capas de Cantones y Parroquias por defecto
-    map.removeLayer(overlayLayers['Bosques y Vegetación']);
-    map.removeLayer(overlayLayers['Provincias']);
-    map.removeLayer(overlayLayers['Cantones']);
-    map.removeLayer(overlayLayers['Parroquias']);
+    ['Bosques y Vegetación', 'Provincias', 'Cantones', 'Parroquias'].forEach(layerName => {
+      map.removeLayer(overlayLayers[layerName]);
+    });
   });
 
-  // Agregamos el marcador draggable
-  var redIcon = new L.Icon({
+  const redIcon = new L.Icon({
     iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
     iconSize: [25, 41],
@@ -87,34 +71,17 @@ function initMap() {
     shadowSize: [41, 41]
   });
 
-  var marker = L.marker([-0.289763, -78.327026], { draggable: true, icon: redIcon }).addTo(map);
+  const marker = L.marker([-0.289763, -78.327026], { draggable: true, icon: redIcon }).addTo(map);
+  marker.bindPopup(`<b>Coordenadas WGS 84</b><br>Latitud: ${marker.getLatLng().lat.toFixed(6)}<br>Longitud: ${marker.getLatLng().lng.toFixed(6)}`).openPopup();
 
-  marker.bindPopup("<b>Coordenadas WGS 84</b><br>Latitud: " + marker.getLatLng().lat.toFixed(6) + "<br>Longitud: " + marker.getLatLng().lng.toFixed(6)).openPopup();
+  marker.on('drag', event => showMarkerCoordinates(marker));
 
-  marker.on('drag', function (event) {
-    showMarkerCoordinates(marker);
-  });
+  document.getElementById('openstreetmap-button').addEventListener('click', () => changeBasemap('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
+  document.getElementById('esri-button').addEventListener('click', () => changeBasemap('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'));
+  document.getElementById('opentopomap-button').addEventListener('click', () => changeBasemap('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'));
+  document.getElementById('CartoDB.DarkMatterNoLabels').addEventListener('click', () => changeBasemap('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png'));
 
-  // Botones para cambiar los basemaps
-  document.getElementById('openstreetmap-button').addEventListener('click', function () {
-    changeBasemap('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-  });
-
-  document.getElementById('esri-button').addEventListener('click', function () {
-    changeBasemap('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}');
-  });
-
-  document.getElementById('opentopomap-button').addEventListener('click', function () {
-    changeBasemap('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png');
-  });
-
-  // Nuevo botón para Carto
-  document.getElementById('CartoDB.DarkMatterNoLabels').addEventListener('click', function () {
-    changeBasemap('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png');
-  });
-
-  // Widget para control de capa de áreas protegidas (en la esquina inferior derecha)
-  var areasProtegidasControl = L.control.layers(null, { 'Áreas Protegidas': overlayLayers['Áreas Protegidas'] }, { position: 'bottomright' });
+  const areasProtegidasControl = L.control.layers(null, { 'Áreas Protegidas': overlayLayers['Áreas Protegidas'] }, { position: 'bottomright' });
   areasProtegidasControl.addTo(map);
 }
 
